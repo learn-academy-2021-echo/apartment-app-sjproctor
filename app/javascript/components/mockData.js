c3c2f300bc7b262fa55653a4d5a84eb4679af352
Sarah Proctor <sproctor950@gmail.com>
2022-02-10 10:26:00 -0800

    adds mock data to react app and passes apartments to index

---
+++ app/javascript/components/mockData.js
@@ -0,0 +1,30 @@
+let apartments = [
+  {
+    id: 1,
+    street: "123 Street",
+    city: "SD",
+    state: "CA",
+    manager: "Joe",
+    email: "joe@testing.com",
+    price: "1000",
+    bedrooms: 2,
+    bathrooms: 3,
+    pets: "all pets welcome",
+    user_id: 1
+  },
+  {
+    id: 2,
+    street: "456 Street",
+    city: "SD",
+    state: "CA",
+    manager: "Joe",
+    email: "joe@testing.com",
+    price: "1000",
+    bedrooms: 2,
+    bathrooms: 3,
+    pets: "no snakes",
+    user_id: 1
+  }
+]
+
+export default apartments
